b0562c460f261323d8cd34555cb1fc5cf1c3285e
Mitchell Pleune <mitchpleune@gmail.com>
2016-10-11 18:08:33 -0400

    ChunkGeneratorPerlin: efficency update
    +Don't try to run through blocks when there is no possibility of hills being generated
    +fill chunks that will be mostly stone with stone then set air speficically, and vice-versa.

--- src/chunkgen_perlin.cpp
+++ src/chunkgen_perlin.cpp
@@ -7,23 +7,40 @@ void ChunkGeneratorPerlin::generate(Chunk *target)
     target->lock(Chunk::WRITE);
     int c_width = Chunk::size();
 
+    long3_t cpos = target->cpos();
+
+    if(cpos.y < 0)
+        target->fill(Block::STONE);
+
     for(int x=0; x<c_width; x++)
     for(int y=0; y<c_width; y++)
     for(int z=0; z<c_width; z++)
     {
-        long3_t cpos = target->cpos();
-
         int X = x + cpos.x*c_width;
         int Y = y + cpos.y*c_width;
         int Z = z + cpos.z*c_width;
 
-        double density =
-            Noise::Perlin::noise(0.01, X, Y, Z) +
-            Noise::Perlin::noise(0.1, X, Y, Z) * .05;
+        if(Y < 120 && y > -120)
+        {
 
+            double mod = Noise::Perlin::noise(0.0002, X, 0, Z) + 1;
+            mod *= 60;
 
-        if(density - ((double)Y/100.0)> 0.0)
-            target->set(x, y, z, Block::STONE);
+            double density =
+                Noise::Perlin::noise(0.01, X, Y, Z) +
+                Noise::Perlin::noise(0.1, X, Y, Z) * .02;
+
+            density -= ((double)Y/(1.0 + mod));
+
+            if(cpos.y < 0)
+            {
+                if(density <= 0)
+                    target->set(x, y, z, Block::AIR);
+            } else {
+                if(density > 0.0)
+                    target->set(x, y, z, Block::STONE);
+            }
+        }
     }
 
     target->unlock();
